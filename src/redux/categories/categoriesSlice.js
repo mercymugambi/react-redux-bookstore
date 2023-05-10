fdc9cee9cf99413552807a87c27e88a41033ea29
mercymugambi <mercymugambi2016@gmail.com>
2023-05-11 00:55:23 +0300

    Modify categories slice

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -2,16 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   categories: [],
-  status: 'Under construction',
 };
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
-    // action
+    status: (state) => {
+      state.categories = ['Under Construction'];
+    },
   },
 });
 
-export const { actions } = categoriesSlice;
+export const { status } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
