5c2629507326fb9cbe2ba13b302d8292c785ff9c
mercymugambi <mercymugambi2016@gmail.com>
2023-05-13 11:43:16 +0300

    Add books categories

--- src/components/Dropdowns.js
+++ src/components/Dropdowns.js
@@ -1,26 +1,23 @@
-// import { PropTypes } from 'prop-types';
-// import { useState } from 'react';
+import { PropTypes } from 'prop-types';
+import { useState } from 'react';
 
-// const Dropdowns = ({ onCategoryChange }) => {
-//   const [category, setCategory] = useState('');
+const Dropdowns = ({ onCategoryChange }) => {
+  const [category, setCategory] = useState('');
 
-//   const handleCategoryChange = (e) => {
-//     const selectedCategory = e.target.value;
-//     setCategory(selectedCategory);
-//     onCategoryChange(selectedCategory);
-//   };
+  const handleCategoryChange = (e) => {
+    const selectedCategory = e.target.value;
+    setCategory(selectedCategory);
+    onCategoryChange(selectedCategory);
+  };
 
-//   return (
-//     <select value={category} onChange={handleCategoryChange} placeholder="Category">
-//       <option value="Action">Action</option>
-//       <option value="Biography">Biography</option>
-//       <option value="History">History</option>
-//       <option value="Horror">Horror</option>
-//       <option value="Kids">Kids</option>
-//       <option value="Learning">Learning</option>
-//       <option value="Sci-Fi">Sci-Fi</option>
-//     </select>
-//   );
-// };
-// Dropdowns.propTypes = { onCategoryChange: PropTypes.func.isRequired };
-// export default Dropdowns;
+  return (
+    <select value={category} onChange={handleCategoryChange} placeholder="Category">
+      <option value="Action">Select Categories</option>
+      <option value="Action">Action</option>
+      <option value="Science">Science Fiction</option>
+      <option value="Economy">Economy</option>
+    </select>
+  );
+};
+Dropdowns.propTypes = { onCategoryChange: PropTypes.func.isRequired };
+export default Dropdowns;
